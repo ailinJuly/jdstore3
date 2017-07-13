2a2b78aac127228f5674059d168666ca21fd8844
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-07-14 07:58:48 +0800

    fix bugs of routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
   root 'products#index'
   namespace :admin do
      resources :products
-     resources :orders
+       resources :orders do
+         member do
+           post :cancel
+           post :ship
+           post :shipped
+           post :return
+       end
+     end
    end
   resources :products do
   member do
@@ -28,12 +35,6 @@ end
   end
 
   namespace :account do
-     resources :orders do
-       post :cancel
-       post :ship
-       post :shipped
-       post :return
-
-     end
+     resources :orders
   end
 end
