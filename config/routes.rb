9a16eee1e49d979d90375ebf4565d18e4451df9a
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-07-14 06:46:22 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'products#index'
   namespace :admin do
      resources :products
+     resources :orders
    end
   resources :products do
   member do
@@ -22,11 +23,12 @@ end
   resources :orders do
     member do
       post :pay_with_alipay
-      post :pay_with_wechat 
+      post :pay_with_wechat
     end
   end
 
   namespace :account do
      resources :orders
+
   end
 end
